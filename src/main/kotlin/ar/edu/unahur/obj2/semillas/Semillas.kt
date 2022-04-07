f97be73d97327f8256456d74575e340d3b0ea120
EnzoAlarcon <enzoalarcon01@gmail.com>
2022-04-07 13:44:19 -0300

    se agrega clases plantas, menta, soja, soja trans, peperina, quinoa. Modifica nombre de metodos

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Semillas.kt
@@ -1,37 +1,73 @@
 package ar.edu.unahur.obj2.semillas
 
-class Menta(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Te la debo, amigue...")
+open class Planta(open var altura: Double, open val anioObtencion: Int) {
+
+    open fun toleranciaAlSol() = 7
+
+    fun esFuerte() : Boolean {
+        return this.toleranciaAlSol() > 9
     }
 
-    fun daSemillas(): Boolean {
-        TODO("Colgue, che...")
+    open fun daNuevasSemillas() : Boolean {
+        return this.esFuerte()
     }
 
-    fun esFuerte(): Boolean {
-        TODO("Estas seguro que lo queres implementar así?")
+}
+
+open class Menta(override var altura: Double, override val anioObtencion: Int) : Planta(altura, anioObtencion) {
+
+    override fun daNuevasSemillas() : Boolean {
+        return super.daNuevasSemillas() || this.altura > 0.4
+    }
+
+    open fun espacioQueOcupa() = this.altura + 1
+
+}
+
+open class Soja (override var altura: Double, override val anioObtencion: Int) : Planta(altura, anioObtencion) {
+
+    override fun toleranciaAlSol(): Int {
+        if (this.altura < 0.5) {return 6}
+        else if (this.altura < 1.0) {return 8}
+        else {return 12}
+    }
+
+    override fun daNuevasSemillas(): Boolean {
+        return super.daNuevasSemillas() || this.anioObtencion < 2007 && (this.altura < 0.9 && this.altura > 0.75)
+    }
+
+    fun espacioQueOcupa() = this.altura / 2
+
+
+}
+
+class Quinoa(var espacioQueOcupa: Double, override val anioObtencion: Int) : Planta(espacioQueOcupa, anioObtencion) {
+
+    override fun toleranciaAlSol(): Int {
+        if (espacioQueOcupa < 0.3) {return 10}
+        else {return super.toleranciaAlSol()}
+    }
+
+    override fun daNuevasSemillas(): Boolean {
+        if (this.anioObtencion > 2001) {return true}
+        else if (this.anioObtencion < 2008) {return true}
+        else {return super.daNuevasSemillas()}
     }
 
 }
 
-class Soja(var altura: Double, val anioSemilla: Int) {
-    fun espacio(): Double {
-        TODO("Lee un poco más el enunciado...")
-    }
+class SojaTransgenica(override var altura: Double, override val anioObtencion: Int) : Soja(altura, anioObtencion) {
 
-    fun daSemillas(): Boolean {
-        TODO("Dalee")
-    }
-
-    fun esFuerte(): Boolean {
-        TODO("Así me implementas mejor")
-    }
-}
-
-class Planta(var altura: Double, val anioSemilla: Int) {
-    fun espacio() {
-        TODO("Pasaron cosas")
+    override fun daNuevasSemillas(): Boolean {
+        return false
     }
 
 }
+
+class Peperina(override var altura: Double, override val anioObtencion: Int) : Menta(altura, anioObtencion) {
+
+    override fun espacioQueOcupa(): Double {
+        return super.espacioQueOcupa() * 2
+    }
+}
+
--- src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/SemillasTest.kt
@@ -15,13 +15,13 @@ class SemillasTest : DescribeSpec ({
 
         it("probamos los atributos altura  y anioSemilla") {
             menta.altura.shouldBe(1.0)
-            menta.anioSemilla.shouldBe(2021)
+            menta.anioObtencion.shouldBe(2021)
         }
 
         it("verificar si da semillas") {
-            menta.daSemillas().shouldBeTrue()
-            mentita.daSemillas().shouldBeFalse()
-            soja.daSemillas().shouldBeFalse()
+            menta.daNuevasSemillas().shouldBeTrue()
+            mentita.daNuevasSemillas().shouldBeFalse()
+            soja.daNuevasSemillas().shouldBeFalse()
         }
 
         it("es fuerte") {
@@ -30,16 +30,16 @@ class SemillasTest : DescribeSpec ({
         }
 
         it("espacio") {
-            menta.espacio().shouldBe(2.0)
-            mentita.espacio().shouldBe(1.3)
-            soja.espacio().shouldBe(0.3)
+            menta.espacioQueOcupa().shouldBe(2.0)
+            mentita.espacioQueOcupa().shouldBe(1.3)
+            soja.espacioQueOcupa().shouldBe(0.3)
         }
 
         it("verifico la suma de varias") {
             val superficie = mutableListOf(
-                soja.espacio(),
-                menta.espacio(),
-                mentita.espacio()
+                soja.espacioQueOcupa(),
+                menta.espacioQueOcupa(),
+                mentita.espacioQueOcupa()
             ).sum()
             Math.ceil(superficie).shouldBe(4)
         }
